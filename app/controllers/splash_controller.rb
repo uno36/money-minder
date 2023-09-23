543e25ae03da9ea56ba34f9332a01c15d139913a
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-23 10:32:39 +0200

    Add controlller code

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,3 +1,9 @@
 class SplashController < ApplicationController
-  def index; end
+  def check_and_redirect
+    if user_signed_in?
+      redirect_to groups_path
+    else
+      render 'splash'
+    end
+  end
 end
