1d2875b495aa40acc3c6464b7b939746922d4a17
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-23 02:50:51 +0200

    Install device

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, :timeoutable,  and :omniauthable
+  devise :database_authenticatable, :trackable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :entities
+  has_many :groups
+  
+  validates :fullname, presence:true
+  validates :name, presence: true       
 end
