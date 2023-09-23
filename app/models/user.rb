53e02f8033396192da14cf6b667c1d3556fd8920
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-23 02:54:02 +0200

    Fix Linter erros

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :entities
   has_many :groups
-  
-  validates :fullname, presence:true
-  validates :name, presence: true       
+
+  validates :fullname, presence: true
+  validates :name, presence: true
 end
